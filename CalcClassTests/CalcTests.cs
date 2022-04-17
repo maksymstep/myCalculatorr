38a9198c04aae33218f64836c0cc4c005090f971
VMatlak <matlack.viktor@gmail.com>
2022-04-17 22:41:55 +0300

    Тести

---
+++ CalcClassTests/CalcTests.cs
@@ -0,0 +1,79 @@
+﻿using CalcClass;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalcClassTests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestClass]
+        public class UnitTest1
+        {
+            [TestMethod]
+            public void Sum_20and45_36returned()
+            {
+                int a = 20;
+                int b = 45;
+                int expected = 65;
+                int actual = Calc.Sum(a, b);
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void Div_9and3_3returned()
+            {
+                int a = 9;
+                int b = 3;
+                int expected = 3;
+                int actual = Calc.Div(a, b);
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod]
+            public void Sub_3and2_1returned()
+            {
+                int a = 3;
+                int b = 2;
+                int expected = 1;
+                int actual = Calc.Sub(a, b);
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void Mult_7and3_21returned()
+            {
+                int a =7;
+                int b = 3;
+                int expected = 21;
+                int actual = Calc.Mul(a, b);
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void Mod_30and3_0returned()
+            {
+                int a = 30;
+                int b = 3;
+                int expected = 0;
+                int actual = Calc.Mod(a, b);
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod]
+            public void M_m8_8returned()
+            {
+                int a = -8;
+                int expected = 8;
+                int actual = Calc.M(a);
+                Assert.AreEqual(expected, actual);
+            }
+            [TestMethod]
+            public void P_8_m8returned()
+            {
+                int a = 8;
+                int expected = -8;
+                int actual = Calc.P(a);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
---
+++ CalcClassTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcClassTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcClassTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6dab8ec3-d3ad-4254-867d-427a9e93fa92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
